fb4ab58364f1043180a561a84692cd2f125449d3
Michael <mpodeke@gmail.com>
2025-08-13 03:43:04 +0100

    added numerous  dependencies

--- obj/Debug/net6.0/AuthTodoApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/AuthTodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthTodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+084a2578df864b3ec2877d7e47e3a5c41db0f43d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+025efd2371706733c327bcfebe45b2aa3e4b480f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthTodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthTodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
